8bae9255ac65d05209d85a90ee24f6f621528dcd
megatkhs <tarodino0526@gmail.com>
2024-04-07 18:11:49 +0900

    不要なファイルを削除

--- src/schedule.ts
+++
@@ -1,9 +0,0 @@
-import cron from "node-cron";
-
-function setMonthlySchedule(callback: () => void) {
-  cron.schedule("0 0 1 * *", callback, {
-    timezone: "Asia/Tokyo",
-  });
-}
-
-export { setMonthlySchedule };
