7ff018b1bb5616bb383c4fefb3b471c3b4eb0526
megatkhs <ryo.takahashi@nextinnovation-inc.co.jp>
2024-07-18 20:16:36 +0900

    envファイルの型定義を追加

---
+++ src/types/bun.d.ts
@@ -0,0 +1,15 @@
+declare module 'bun' {
+  interface Env {
+    DISCORD_TOKEN: string
+    DISCORD_GUILD_ID: string
+    DISCORD_CHANNEL_ID: string
+    YOUTUBE_API_CLIENT_ID: string
+    YOUTUBE_API_CLIENT_SECRET: string
+    YOUTUBE_API_REDIRECT_URL: string
+    MICROCMS_SERVICE_DOMAIN: string
+    MICROCMS_API_KEY: string
+    REVALIDATE_URL: string
+    REVALIDATE_SECRET: string
+    DATABASE_URL: string
+  }
+}
