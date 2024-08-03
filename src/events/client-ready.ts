582c2fea91c1f1a484428b8f1e884759fcaf2c0f
megatkhs <tarodino0526@gmail.com>
2024-08-03 12:51:04 +0900

    Botが起動した際に管理者チャンネルにも起動通知を行うように変更

--- src/events/client-ready.ts
+++ src/events/client-ready.ts
@@ -5,17 +5,34 @@ import { ChannelType, DiscordAPIError, Events } from 'discord.js'
 export default defineEvent({
   name: Events.ClientReady,
   async once(client) {
-    console.log(`${client.user.tag}が正常に起動しました。`)
+    console.log('[ready]', `${client.user.tag}が正常に起動しました。`)
 
-    console.log('チャンネルに通知を送っています...')
     const notificationChannels = await prisma.channel.findMany({
       where: {
-        wakeUpNotification: true,
+        OR: [
+          {
+            wakeUpNotification: true,
+          },
+          // 管理者チャンネルにも通知を行う
+          {
+            id: Bun.env.DISCORD_ADMIN_CHANNEL_ID,
+          },
+        ],
       },
       select: {
         originalId: true,
       },
     })
+
+    if (notificationChannels.length === 0) {
+      console.log(
+        '[ready]',
+        'Botの起動を通知するチャンネルがありませんでした。',
+      )
+      return
+    }
+
+    console.log('[ready]', 'チャンネルに通知を送っています...')
     await Promise.all(
       notificationChannels.map(async ({ originalId: channelId }) => {
         try {
@@ -24,18 +41,20 @@ export default defineEvent({
             await channel.send(`\`${client.user.tag}\` が正常に起動しました。`)
           }
         } catch (error) {
-          console.error('メッセージ送信に失敗しました。')
+          console.error('[ready]', 'メッセージ送信に失敗しました。')
 
           if (error instanceof DiscordAPIError) {
             switch (error.code) {
               case 10003: // Unknown Channel
                 console.error(
+                  '[ready]',
                   `\`channelId:${channelId}\`が見つかりませんでした。`,
                 )
                 return
 
               case 50001: // Missing Access
                 console.error(
+                  '[ready]',
                   `\`channelId:${channelId}\`にアクセスする権限がありませんでした。`,
                 )
                 return
@@ -46,6 +65,6 @@ export default defineEvent({
         }
       }),
     )
-    console.log('チャンネルに通知を送信しました。')
+    console.log('[ready]', 'チャンネルに通知を送信しました。')
   },
 })
--- src/types/bun.d.ts
+++ src/types/bun.d.ts
@@ -4,6 +4,7 @@ declare module 'bun' {
     DISCORD_GUILD_ID: string
     DISCORD_CHANNEL_ID: string
     DISCORD_CLIENT_ID: string
+    DISCORD_ADMIN_CHANNEL_ID?: string
     YOUTUBE_API_CLIENT_ID: string
     YOUTUBE_API_CLIENT_SECRET: string
     YOUTUBE_API_REDIRECT_URL: string
