d65db25fd7acfa8e8ac9eecc539b9e1a0cd8504c
megatkhs <ryo.takahashi@megatkhs.com>
2026-05-06 23:22:38 +0900

    ci: generate Wrangler types in CI/CD pipelines

--- apps/api/src/types.ts
+++ apps/api/src/types.ts
@@ -1,13 +1,7 @@
 import { DrizzleD1Database } from "drizzle-orm/d1";
 import * as schema from "./db/schema";
 
-export type Bindings = {
-  DB: D1Database;
-  API_KEY: string;
-  YOUTUBE_CLIENT_ID: string;
-  YOUTUBE_CLIENT_SECRET: string;
-  YOUTUBE_REDIRECT_URL: string;
-};
+export type Bindings = Cloudflare.Env
 
 export type Variables = {
   db: DrizzleD1Database<typeof schema>;
