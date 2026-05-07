e40aebf068551f59b5d322d110c0ba2361657c4a
megatkhs <ryo.takahashi@megatkhs.com>
2026-05-07 11:44:23 +0900

    feat: add credential relationship to channels table, configure drizzle, and set up API deployment workflow

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,13 +1,34 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
+import path from "node:path";
+import fs from "node:fs";
 
 dotenv.config();
+
+function getLocalD1DB(): string {
+  try {
+    const basePath = path.resolve(".wrangler/state/v3/d1/miniflare-D1DatabaseObject");
+    const dbFile = fs
+      .readdirSync(basePath, { encoding: "utf-8", recursive: true })
+      .find((f) => f.endsWith(".sqlite"));
+
+    if (!dbFile) {
+      throw new Error(`.sqlite file not found in ${basePath}`);
+    }
+
+    const url = path.resolve(basePath, dbFile);
+    return url;
+  } catch (err) {
+    console.error(`Error  ${err}`);
+    process.exit(1);
+  }
+}
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "./local.sqlite"
+    url: getLocalD1DB(),
   }
 });
